eec0747c628561a385cf978e6861d938b2f3044f
ArjunT1 <arjun.baiju@gmail.com>
2026-08-31 16:38:58 -0400

    This is a new script that adds a green segment to better show the geometry of our robot that we got at the end of summer. It features like a (estimated with string tech) CoM location as well

---
+++ InvPendulumForelimb4.m
@@ -0,0 +1,151 @@
+clear;
+
+
+mass = 1; %kg
+l_inches = 7.125;
+l = l_inches/39.3701; %inches converted to meters
+arm_inches = 5.5;
+arm = arm_inches/39.3701;
+g = 9.81; %m/s
+
+extended_l = 13 / 39.3701;
+%extended_arm =  / 39.3701;
+
+height = 3.5 / 39.3701;
+
+theta0 = -asin(arm/l);
+theta0_deg = rad2deg(theta0);
+%theta0 = deg2rad(-5); %the number is degrees in it
+omega0 = 0;
+
+phi = pi/2 + theta0;
+%phi = deg2rad(85); %the number is degrees in it
+%Right now im assuming whatever the angle we are falling away from the
+%tree, the forelimbs are at a position where they hit the tree
+%perpendicular to the tree
+
+state0 = [theta0; omega0];
+
+time = [0 0.55];
+
+dt = 0.01;
+tspan = 0:dt:0.55;
+
+[t, x] = ode45(@(t,x) invPendulum(x, l, g), tspan, state0);
+
+theta = x(:,1);
+theta_dot = x(:,2);
+
+s = zeros(length(theta), 1);
+forearm_inches = zeros(length(theta), 1);
+
+alpha = zeros(length(theta), 1);
+beta = zeros(length(theta), 1);
+xc = zeros(length(theta), 1);
+yc = zeros(length(theta), 1);
+xup = zeros(length(theta), 1);
+yup = zeros(length(theta), 1);
+xp = zeros(length(theta), 1);
+yp = zeros(length(theta), 1);
+
+figure(1);
+for i = 1:length(t)
+
+    clf;
+    hold on;
+
+    th = theta(i);
+
+    % Pendulum tip
+    xp(i) = l*sin(th);
+    yp(i) = l*cos(th);
+    
+    % Offset segment direction
+    alpha(i) = -th + phi - pi/2;
+
+    %Noodling for the green mid segment
+    beta(i) = -th + phi;
+    xup(i) = xp(i) + height*cos(beta(i));
+    yup(i) = yp(i) + height*sin(beta(i));
+ 
+    % Solve for intersection with x = 0
+    s(i) = -xup(i) / cos(alpha(i));
+
+    % Endpoint on vertical axis
+    xc(i) = xup(i) + s(i)*cos(alpha(i));
+    yc(i) = yup(i) + s(i)*sin(alpha(i));
+
+    if yc(i) < 0.0
+        time_disaster = t(i);
+        break
+    end
+
+    forearm_inches(i) = s(i) * 39.3701;
+
+    % Draw tree line
+    plot([0 0], [-1.5 1.5], 'k--');
+
+    % Draw pendulum
+    plot([0 xp(i)], [0 yp(i)], ...
+        'b', 'LineWidth', 3);
+
+    % Draw up haha
+    plot([xp(i) xup(i)], [yp(i) yup(i)], ...
+        'g', 'LineWidth', 3);
+
+    % Draw to tree
+    plot([xup(i) xc(i)], [yup(i) yc(i)], ...
+        'r', 'LineWidth', 3);
+
+    % Draw pendulum mass
+    plot(xp(i), yp(i), ...
+        'ko', ...
+        'MarkerFaceColor', 'k', ...
+        'MarkerSize', 10);
+
+    axis equal;
+    axis([-.75 .75 -.75 .75]);
+
+    title(sprintf('t = %.2f s', t(i)));
+
+    grid on;
+
+    drawnow;
+end
+
+
+cooked_angle = rad2deg(theta) + rad2deg(phi);
+
+forearm_extension = forearm_inches - 5.5;
+
+% Plot angle
+figure(3);
+plot(t, rad2deg(theta), 'LineWidth', 2);
+xlabel('Time (s)');
+ylabel('\theta (deg)');
+title('Inverted Pendulum Fall');
+grid on;
+
+%final variable which shows at any given time what the forearm length should
+%be, at time 0 it should be that length right when we begin falling
+time_alphadeg_forearm_xy = [t rad2deg(alpha) forearm_extension xp*39.3701 (yc-yp(1))*39.3701];
+
+figure(2);
+plot(t, forearm_extension, 'LineWidth', 2);
+xlabel('Time (s)');
+ylabel('Forelimb Extension (inches)');
+title('Forelimb Extension');
+grid on;
+
+alpha_deg = rad2deg(alpha);
+
+function dx = invPendulum(x, l, g)
+
+theta = x(1);
+theta_dot = x(2);
+
+theta_ddot = (g/l)*sin(theta);
+
+dx = [theta_dot;
+      theta_ddot];
+end
